800fec59f41a4e153f580d858fb2418ecbf2223b
Fefer <ffalves9889@gmail.com>
2024-11-08 10:41:56 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    const menu = document.querySelector(".menu-list");
+    menu.style.display = menu.style.display === "block" ? "none" : "block";
+  });
+  
